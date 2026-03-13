66c1df11f0d0d045d550960e1bacaedbda2ecefa
Arsabxx <gcchen0516@gmail.com>
2026-03-13 09:22:24 +0000

    Update sketch_nov25a.ino

--- sketch_nov25a.ino
+++ sketch_nov25a.ino
@@ -1,10 +1,32 @@
+const int POT    = A2;
+const int LED1   = 13;
+const int LED2   = 3;
+const int LED3   = 5;
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println("KY-039 Raw Waveform - 開始夾手指 30-60秒");
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
 }
 
 void loop() {
-  int raw = analogRead(A0);
-  Serial.println(raw);  // 只 print 數字
-  delay(20);            // 50Hz 採樣
+  int potValue = analogRead(POT);
+  long led2Time = map(potValue, 0, 1023, 1000, 2000);
+
+  digitalWrite(LED1, HIGH);
+  delay(1100);
+  digitalWrite(LED1, LOW);
+
+  digitalWrite(LED2, HIGH);
+  delay(led2Time);
+  digitalWrite(LED2, LOW);
+
+  digitalWrite(LED3, HIGH);
+  delay(1900);
+  digitalWrite(LED3, LOW);
+
 }
