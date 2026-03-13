0306fcd30b425a5c9f7288911380b1eb39596200
Arsabxx <gcchen0516@gmail.com>
2026-03-13 09:36:31 +0000

    Update sketch_nov25a.ino

--- sketch_nov25a.ino
+++ sketch_nov25a.ino
@@ -3,6 +3,17 @@ const int LED1   = 13;
 const int LED2   = 3;
 const int LED3   = 5;
 
+unsigned long previousMillis = 0;     
+unsigned long cycleInterval = 6000; 
+
+bool led1On = false;
+bool led2On = false;
+bool led3On = false;
+
+unsigned long led1Duration = 1100;   
+unsigned long led3Duration = 1900;    
+unsigned long led2Duration = 1500;  
+
 void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -14,19 +25,36 @@ void setup() {
 }
 
 void loop() {
-  int potValue = analogRead(POT);
-  long led2Time = map(potValue, 0, 1023, 1000, 2000);
+  unsigned long currentMillis = millis();
 
-  digitalWrite(LED1, HIGH);
-  delay(1100);
-  digitalWrite(LED1, LOW);
+  if (currentMillis - previousMillis >= cycleInterval) {
+    previousMillis = currentMillis;
 
-  digitalWrite(LED2, HIGH);
-  delay(led2Time);
-  digitalWrite(LED2, LOW);
+    int potValue = analogRead(POT);
+    led2Duration = map(potValue, 0, 1023, 1000, 2000);
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
 
-  digitalWrite(LED3, HIGH);
-  delay(1900);
-  digitalWrite(LED3, LOW);
+    led1On = true;
+    led2On = true;
+    led3On = true;
+  }
+
+  if (led1On && (currentMillis - previousMillis >= led1Duration)) {
+    digitalWrite(LED1, LOW);
+    led1On = false;
+  }
+
+  if (led2On && (currentMillis - previousMillis >= led2Duration)) {
+    digitalWrite(LED2, LOW);
+    led2On = false;
+  }
+
+  if (led3On && (currentMillis - previousMillis >= led3Duration)) {
+    digitalWrite(LED3, LOW);
+    led3On = false;
+  }
+}
 
 }
