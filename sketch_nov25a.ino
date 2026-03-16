51c7d7054507bcd12c7d8e2b99774df5b789be3c
Arsabxx <gcchen0516@gmail.com>
2026-03-16 15:02:23 +0000

    Update sketch_nov25a.ino

--- sketch_nov25a.ino
+++ sketch_nov25a.ino
@@ -1,34 +1,25 @@
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino: In function 'void setup()':
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:45:11: error: 'LED_PIN' was not declared in this scope
-   pinMode(LED_PIN, OUTPUT);
-           ^~~~~~~
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:45:11: note: suggested alternative: 'SS_PIN'
-   pinMode(LED_PIN, OUTPUT);
-           ^~~~~~~
-           SS_PIN
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino: In function 'void loop()':
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:65:18: error: 'LED_PIN' was not declared in this scope
-     digitalWrite(LED_PIN, HIGH);                // Turn on LED to indicate "scan card"
-                  ^~~~~~~
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:65:18: note: suggested alternative: 'SS_PIN'
-     digitalWrite(LED_PIN, HIGH);                // Turn on LED to indicate "scan card"
-                  ^~~~~~~
-                  SS_PIN
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:96:22: error: 'LED_PIN' was not declared in this scope
-         digitalWrite(LED_PIN, LOW);
-                      ^~~~~~~
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:96:22: note: suggested alternative: 'SS_PIN'
-         digitalWrite(LED_PIN, LOW);
-                      ^~~~~~~
-                      SS_PIN
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino: In function 'int getDistance()':
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:124:29: error: 'THERMISTOR_PIN' was not declared in this scope
-   int rawValue = analogRead(THERMISTOR_PIN);
-                             ^~~~~~~~~~~~~~
-C:\Users\29238929\AppData\Local\Temp\.arduinoIDE-unsaved2026214-12916-15qv9o0.ub4z\sketch_mar14a\sketch_mar14a.ino:124:29: note: suggested alternative: 'TRIG_PIN'
-   int rawValue = analogRead(THERMISTOR_PIN);
-                             ^~~~~~~~~~~~~~
-                             TRIG_PIN
-exit status 1
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-Compilation error: 'LED_PIN' was not declared in this scope
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1  // 或 你的 reset pin，如果有
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(115200);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // 改 0x3D 如果 scanner 顯示
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println(F("Hello OLED!"));
+  display.println(F("Test 123"));
+  display.display();
+}
+
+void loop() {}
