6a65355c1cd1c42d873db8e2c2f963038db86a6a
Arsabxx <gcchen0516@gmail.com>
2026-04-24 09:50:13 +0100

    Refactor WiFi web server sketch for LED control

--- sketch_nov25a.ino
+++ sketch_nov25a.ino
@@ -1,167 +1,90 @@
-/*
-  WiFi Web Server LED Blink
+#include <WiFiS3.h>          
 
-  A simple web server that lets you blink an LED via the web.
-  This sketch will create a new access point (with no password).
-  It will then launch a new server and print out the IP address
-  to the Serial Monitor. From there, you can open that address in a web browser
-  to turn on and off the LED on pin 13.
+const char* ssid = "TP-Link_4B2C";           
+const char* password = "43545148";   
+const char* switchIP = "192.168.0.181";        
+const int   switchPort = 9999;          
+const int buttonPin = 2;                       
 
-  If the IP address of your board is yourAddress:
-    http://yourAddress/H turns the LED on
-    http://yourAddress/L turns it off
-
-  created 25 Nov 2012
-  by Tom Igoe
-  adapted to WiFi AP by Adafruit
-
-  Find the full UNO R4 WiFi Network documentation here:
-  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#access-point
- */
-
-#include "WiFiS3.h"
-
-#include "arduino_secrets.h" 
-
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;        // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key index number (needed only for WEP)
-
-int led =  LED_BUILTIN;
-int status = WL_IDLE_STATUS;
-WiFiServer server(80);
+// Variables for button (debounce)
+bool lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50;        // 50ms debounce
 
 void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("Access Point Web Server");
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP);            // Use internal pull-up
 
-  pinMode(led, OUTPUT);      // set the LED pin mode
+  Serial.println("\nSwitch Controller");
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi: ");
+  Serial.print(ssid);
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
 
-  // by default the local IP address will be 192.168.4.1
-  // you can override it with the following:
-  WiFi.config(IPAddress(192,48,56,2));
-
-  // print the network name (SSID);
-  Serial.print("Creating access point named: ");
-  Serial.println(ssid);
-
-  // Create open network. Change this line if you want to create an WEP network:
-  status = WiFi.beginAP(ssid, pass);
-  if (status != WL_AP_LISTENING) {
-    Serial.println("Creating access point failed");
-    // don't continue
-    while (true);
-  }
-
-  // wait 10 seconds for connection:
-  delay(10000);
-
-  // start the web server on port 80
-  server.begin();
-
-  // you're connected now, so print out the status
-  printWiFiStatus();
+  Serial.println("\nWiFi Connected!");
+  Serial.print("Uno R4 IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Target Switch IP: ");
+  Serial.println(switchIP);
+  Serial.println("Press the button to toggle the switch.");
 }
-
 
 void loop() {
-  
-  // compare the previous status to the current status
-  if (status != WiFi.status()) {
-    // it has changed update the variable
-    status = WiFi.status();
+  bool buttonState = digitalRead(buttonPin);
 
-    if (status == WL_AP_CONNECTED) {
-      // a device has connected to the AP
-      Serial.println("Device connected to AP");
-    } else {
-      // a device has disconnected from the AP, and we are back in listening mode
-      Serial.println("Device disconnected from AP");
-    }
+  // Detect button press (falling edge + debounce)
+  if (buttonState == LOW && lastButtonState == HIGH && 
+      (millis() - lastDebounceTime) > debounceDelay) {
+    
+    lastDebounceTime = millis();
+    Serial.println("\nToggling Switch...");
+
+    toggleKasaSwitch();
   }
-  
-  WiFiClient client = server.available();   // listen for incoming clients
 
-  if (client) {                             // if you get a client,
-    Serial.println("new client");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      delayMicroseconds(10);                // This is required for the Arduino Nano RP2040 Connect - otherwise it will loop so fast that SPI will never be served.
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out to the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+  lastButtonState = buttonState;
+  delay(10);
+}
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
+void toggleKasaSwitch() {
+  WiFiClient client;
 
-            // the content of the HTTP response follows the header:
-            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/H\">here</a> turn the LED on<br></p>");
-            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/L\">here</a> turn the LED off<br></p>");
+  if (client.connect(switchIP, switchPort)) {
+    Serial.println("Connected to switch... sending toggle command");
 
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          }
-          else {      // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        }
-        else if (c != '\r') {    // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
+    const char* cmd = "{\"system\":{\"set_relay_state\":{\"state\":-1}}}";  // -1 = toggle
 
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(led, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(led, LOW);                // GET /L turns the LED off
-        }
-      }
-    }
-    // close the connection:
+    // Encrypt the command (TP-Link Kasa uses simple XOR 0xAB)
+    String encrypted = encryptKasa(cmd);
+
+    client.print(encrypted);
     client.stop();
-    Serial.println("client disconnected");
+
+    Serial.println("Toggle command sent successfully!");
+  } 
+  else {
+    Serial.println("Connection failed! Check switch IP and WiFi.");
   }
 }
 
-void printWiFiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+String encryptKasa(const char* str) {
+  String result = "";
+  int key = 0xAB;                     // Fixed key for Kasa local protocol
+  int len = strlen(str);
 
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  result += (char)((len >> 8) & 0xFF);
+  result += (char)(len & 0xFF);
 
-  // print where to go in a browser:
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
-
+  for (int i = 0; i < len; i++) {
+    result += (char)(str[i] ^ key);
+    key = result[result.length() - 1];   // Update key for next byte
+  }
+  return result;
+}
 }
